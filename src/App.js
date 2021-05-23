84d7bc481fc93f20eed773f53186087c00bde80c
Nati Kamusher <natan.kamusher@gmail.com>
2021-05-23 19:54:51 +0300

    Added single post page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import { Navbar } from './app/Navbar'
 import { AddPostForm } from './features/AddPostForm'
 import { PostsList } from './features/PostsList'
+import { SinglePostPage } from './features/SinglePostPage'
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Redirect to="/" />
         </Switch>
       </div>
